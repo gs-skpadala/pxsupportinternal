9b699894746128bacfeec309a3098537f55b278a
Sai Kumar Padala <sai24kumar24@gmail.com>
2024-07-31 00:45:24 +0530

    added 2 buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,8 @@ const Home = () => {
       <button>Button 1</button>
       <button>Button 2</button>
       <button>Button 3</button>
+      <button>Button 4</button>
+      <button>Button 5</button>
     </div>
   );
 };
