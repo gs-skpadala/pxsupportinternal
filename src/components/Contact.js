5b84e46c3d9edfeefefd81317e79d08a6172801d
Sai Kumar Padala <sai24kumar24@gmail.com>
2024-08-06 15:25:09 +0530

    added 5th button to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,6 +13,7 @@ const Contact = () => {
         <button>Contact Button 2</button>
         <button>Contact Button 3</button>
         <button>Contact Button 4</button> 
+        <button>Contact Button 5 added</button> 
       </div>
   );
 };
