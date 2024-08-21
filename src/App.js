e62fca17b6c5532b257619cafb2be31a54882bf6
gs-msreevani <msreevani@gainsight.com>
2024-08-21 12:37:23 +0530

    Update App.js with NotFound Route

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Contact from './components/Contact';
 import LoginForm from './components/LoginForm';
 import Logout from './components/Logout';
 import Dropdown from './components/Dropdown';
+import NotFoundPage from './components/NotFoundPage';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -53,6 +54,7 @@ const App = () => {
           <Route path="/dropdown" element={isLoggedIn ? <Dropdown /> : <Navigate to="/" />} />
           <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
           {/* Fallback route for incorrect URLs */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
 
 
